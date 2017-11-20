d5a8c22890aebceeb63331e34c85a47e899b60c7
cetiniz <cetiniz@mcmaster.ca>
2017-11-20 16:48:33 -0500

    Added Transitions

--- Parking_Meter/App.xaml.cs
+++ Parking_Meter/App.xaml.cs
@@ -69,7 +69,7 @@ namespace Parking_Meter
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(NavigatePage), e.Arguments);
+                    rootFrame.Navigate(typeof(StartPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
